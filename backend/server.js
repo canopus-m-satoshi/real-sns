17b5a869e4d6697b47fbf00ee0794781db64e74d
Satoshi Matsushima <>
2022-06-24 22:32:50 +0900

    10. 認証用と投稿用ルーティング設定もしておこう

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express() // express関数を呼び出し、appでexpressのクラスなどを利用できるようにする
 const userRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
+const postRouter = require('./routes/posts')
 const PORT = 5000
 
 // ミドルウェア
 app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
 
 app.listen(PORT, () => console.log('サーバーが起動しました'))
