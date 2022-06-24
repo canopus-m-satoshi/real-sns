32e5be5d5569c454b1e670eed047908bf6387d7d
Satoshi Matsushima <>
2022-06-24 22:21:56 +0900

    8. Expressフレームワークでローカルサーバーを立ち上げてみよう

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const express = require('express')
+const app = express() // express関数を呼び出し、appでexpressのクラスなどを利用できるようにする
+const PORT = 5000
+
+app.get('/', (req, res) => {
+  res.send('hello express')
+})
+
+app.listen(PORT, () => console.log('サーバーが起動しました'))
