fa744ed75edbe2b649e627c957ad39ffec4fdea4
Satoshi Matsushima <>
2022-06-24 22:29:52 +0900

    9. ルーティング設定をしてエンドポイントを手軽に変更してみよう

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express() // express関数を呼び出し、appでexpressのクラスなどを利用できるようにする
+const userRouter = require('./routes/users')
 const PORT = 5000
 
-app.get('/', (req, res) => {
-  res.send('hello express')
-})
+// ミドルウェア
+app.use('/api/users', userRouter)
 
 app.listen(PORT, () => console.log('サーバーが起動しました'))
