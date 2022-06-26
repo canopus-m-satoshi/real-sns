af05421531c6f3a9eedf1554742d777b6e86a6e2
Satoshi Matsushima <>
2022-06-26 14:51:48 +0900

    32. フォローしているユーザーの投稿と自分の投稿を取得するAPIを自作しよう

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const User = require('../models/User')
 
 // 投稿を作成する
 router.post('/', async (req, res) => {
@@ -82,6 +83,26 @@ router.put('/:id/like', async (req, res) => {
     }
   } catch (err) {
     return res.status(500).json(err)
+  }
+})
+
+// タイムラインの投稿を取得
+router.get('/timeline/all', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.params.userId)
+    const userPosts = await Post.find({ userId: currentUser._id })
+
+    // 自分がフォローしている友達の投稿内容を全て取得する
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId })
+      }),
+    )
+
+    return res.status(200).json(userPosts.concat(...friendPosts)) // concat() ---> 2つ以上の配列を結合する
+    // ...friendPosts　とスプレッド構文にしているのは、friendPostsが配列でデータを格納しておりその格納されているデータを一つずつ取り出すため
+  } catch (err) {
+    return res.json(500).json(err)
   }
 })
 
