bc01e0a5111cdb4cc8c2dedcabc43fc1fc91b9f5
Satoshi Matsushima <>
2022-06-26 14:28:46 +0900

    24. ユーザーをフォロー解除するAPIを自作してみよう

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -85,4 +85,37 @@ router.put('/:id/follow', async (req, res) => {
   }
 })
 
+// ユーザーのフォローを外す
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id) // フォローしたいユーザーID
+      const currentUser = await User.findById(req.body.userId) // 操作をしているユーザーID
+
+      // フォロワーに存在したらフォローを外せる
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          },
+        })
+
+        await currentUser.updateOne({
+          $pull: {
+            followings: req.params.id,
+          },
+        })
+
+        return res.status(200).json('フォローを解除しました')
+      } else {
+        return res.status(403).json('このユーザーはフォロー解除できません')
+      }
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(500).json('自分のアカウントです')
+  }
+})
+
 module.exports = router
