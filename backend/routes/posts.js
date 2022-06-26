3081ff3571684514e1485f4e676b1f427af639e3
Satoshi Matsushima <>
2022-06-26 14:37:22 +0900

    27. 投稿を編集するAPIを自作しよう

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -13,4 +13,21 @@ router.post('/', async (req, res) => {
   }
 })
 
+// 投稿を更新する
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      })
+      return res.status(200).json('投稿内容を更新しました')
+    } else {
+      return res.status(403).json('あなたは他の人の投稿を編集できません')
+    }
+  } catch (err) {
+    return res.status(403).json(err)
+  }
+})
+
 module.exports = router
