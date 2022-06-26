ede5b238be89a322ba018aae9668ed44186031a3
Satoshi Matsushima <>
2022-06-26 14:13:39 +0900

    20. ユーザー情報を削除するAPIを自作してみよう

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,4 +21,20 @@ router.put('/:id', async (req, res) => {
   }
 })
 
+// ユーザー情報の削除
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id)
+      res.status(200).json('ユーザー情報が削除されました')
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res
+      .status(403)
+      .json('あなたは自分のアカウントの時だけユーザー情報を更新できます')
+  }
+})
+
 module.exports = router
