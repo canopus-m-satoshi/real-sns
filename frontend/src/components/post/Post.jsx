a8fc2aa5c0f723d8f038b0e10ab3aa1b7091f22e
Satoshi Matsushima <>
2022-06-28 00:07:22 +0900

    59. ダミーデータで投稿内容を更新しよう

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -3,7 +3,7 @@ import { MoreVert } from '@mui/icons-material'
 import React from 'react'
 import './Post.css'
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <div className="post">
       <div className="postWrapper">
@@ -14,24 +14,26 @@ const Post = () => {
               alt=""
               className="postProfileImg"
             />
-            <span className="postUsername">Shincode</span>
-            <span className="postDate">5分前</span>
+            <span className="postUsername">{post.username}</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">SNS自作中です</span>
-          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+          <span className="postText">{post.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img src="/assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">150人がいいねを押しました</span>
+            <span className="postLikeCounter">
+              {post.like}人がいいねを押しました
+            </span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">400:コメント</span>
+            <span className="postCommentText">{post.comment}:コメント</span>
           </div>
         </div>
       </div>
--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -4,12 +4,16 @@ import Share from '../share/Share'
 import Post from '../post/Post'
 import './Timeline.css'
 
+import { Posts } from '../../dummydata'
+
 const Timeline = () => {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        <Post />
+        {Posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
       </div>
     </div>
   )
