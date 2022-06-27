3ee9a5d9964efea0849633dd8fdf648daa33084d
Satoshi Matsushima <>
2022-06-27 23:06:30 +0900

    49. TimeLineコンポーネントの雛形を作成しよう

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 import './Timeline.css'
 
 const Timeline = () => {
-  return <div className="timeline"></div>
+  return (
+    <div className="timeline">
+      <div className="timelineWrapper">
+        <Share />
+      </div>
+    </div>
+  )
 }
 
 export default Timeline
